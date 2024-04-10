1c721a6c40fe3b64b4d238b6d95609392c62f11a
Rama Koneru <koneru396@gmail.com>
2024-04-09 23:31:17 -0500

    Copilot Side Pane Demo resources

---
+++ open-side-pane.js
@@ -0,0 +1,33 @@
+
+var RRK = RRK || {};
+// Add a function to the namespace
+RRK.openSidePaneWR = function (paneId, paneTitle, canClosePane, webResourceName, recordId, imageSrc) {
+
+
+    var pageInput = {
+        pageType: "webresource",
+        webresourceName: webResourceName,
+        data: encodeURIComponent(`recordId=${recordId}`)
+    };
+
+    var sidePane = Xrm.App.sidePanes.getPane(paneId);
+    if (sidePane) {//If the pane is already open, just navigate to the page
+        sidePane.navigate(pageInput);
+        return;
+    }
+
+
+    var paneOptions = {
+        paneId: paneId,
+        title: paneTitle,
+        imageSrc:imageSrc,
+        canClose: canClosePane
+    };
+
+    Xrm.App.sidePanes.createPane(paneOptions).then((pane) => {
+        pane.navigate(pageInput);
+    });
+
+}
+
+
